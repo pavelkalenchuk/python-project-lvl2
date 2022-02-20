2320002bac0cc4870104e24f24f1467752756c5a
Pavel Kalenchuk <pavel.kalenchuk@gmail.com>
2022-02-21 00:02:37 +0300

    refactor test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,9 @@
 from gendiff.engine import generate_diff
 
 
-file1 = '/home/pavel/projects/python-project-lvl2/tests/fixtures/file1.json'
-file2 = '/home/pavel/projects/python-project-lvl2/tests/fixtures/file2.json'
-result_file = '/home/pavel/projects/python-project-lvl2/tests/fixtures/result.txt'
+file1 = 'tests/fixtures/file1.json'
+file2 = 'tests/fixtures/file2.json'
+result_file = 'tests/fixtures/result.txt'
 
 
 f = open(result_file, 'r')
